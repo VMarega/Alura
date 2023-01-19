3d28f1d526c685338601ebcc48ed3a8a4bdd47ad
VMarega <vinimarega@gmail.com>
2023-01-19 09:39:40 -0300

    new

---
+++ javascript_para_web/Ex1/main.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector("#calcular");
+const resultado = document.querySelector(".resultado");
+
+botao.addEventListener("click", (evento) =>{
+    resultado.innerHTML = "Fui Clicado"
+})
+
---
+++ javascript_para_web/Robotron/js/main.js
@@ -0,0 +1,21 @@
+
+
+const controle = document.querySelectorAll("[data-controle]");
+
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento)=> {
+        manipuladados(evento.target.dataset.controle, evento.target.parentNode)
+    })
+})
+
+
+function manipuladados(operacao, controle){
+    const peca = controle.querySelector("[data-contador]");
+
+
+    if(operacao === "-"){
+        peca.value = parseInt(peca.value) - 1;
+    } else {
+        peca.value = parseInt(peca.value) +1;
+    }
+}
