087b445e05dd595e4cc713ef7667cfdba26aee4a
VMarega <vinimarega@gmail.com>
2023-04-04 10:12:14 -0300

    setters

--- js_orientacao_objetos/User.js
+++ js_orientacao_objetos/User.js
@@ -1,19 +1,25 @@
 export default class User{
     #nome
+    #sobrenome
     #email
     #nascimento
     #role
     #ativo
-    constructor(nome, email, nascimento, role, ativo = true){
+    constructor(nome, sobrenome, email, nascimento, role, ativo = true){
         this.#nome = nome
+        this.#sobrenome = sobrenome
         this.#email = email
         this.#nascimento = nascimento
         this.#role = role || "estudante"
         this.#ativo = ativo
     }
 
-    get nome(){
-        return this.#nome
+    get nome() {
+        return `${this.#nome} ${this.#sobrenome}`
+      }
+
+    get sobrenome(){
+        return this.#sobrenome
     }
 
    
@@ -33,9 +39,18 @@ export default class User{
         return this.#ativo
     }
 
+    set nome(novoNome){
+        if(novoNome == " "){
+            throw new Error("Formato não válido")
+        }
+        let [nome, ...sobrenome] = novoNome.split(" ")
+        sobrenome = sobrenome.join(" ")
+        this.#nome = nome
+        this.#sobrenome = sobrenome
+    }
+
     exibirInfos (){
-        const objUser = this.#montaObjUser()
-        return `${objUser.nome}, ${objUser.email}`
+        return `${this.nome}, ${this.email}`
     }
 }
 
--- js_orientacao_objetos/index.js
+++ js_orientacao_objetos/index.js
@@ -3,5 +3,13 @@ import Docente from "./Docente.js";
 import Admin from "./Admin.js";
 
 
-const novoAdmin = new Admin ("Rodrigo", "r@r.com", "2021-01-01")
+/*const novoAdmin = new Admin ("Rodrigo", "r@r.com", "2021-01-01")
 console.log(novoAdmin.nome)
+novoAdmin.nome = " "
+console.log(novoAdmin.nome)*/
+
+const novoUser = new User("Juliana", "Souza", "j@j.com", "2021-01-01")
+console.log(novoUser.nome)
+novoUser.nome = "Juliana Silva Souza"
+console.log(novoUser.nome)
+console.log(novoUser.sobrenome)
