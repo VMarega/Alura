8edbb7cb397ba2a090de621329b0f9d971c09038
VMarega <vinimarega@gmail.com>
2023-04-04 19:36:39 -0300

    regex

--- primeira_biblioteca_node/2708-node-lib-md-arquivos-iniciais/2708-node-lib-md-arquivos-iniciais/index.js
+++ primeira_biblioteca_node/2708-node-lib-md-arquivos-iniciais/2708-node-lib-md-arquivos-iniciais/index.js
@@ -1,6 +1,14 @@
 import fs from 'fs';
 import chalk from "chalk";
 
+function extraiLinks(texto){
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}));
+    return resultados;
+}
+
+
 function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'NÃO HÁ ARQUIVO NO DIRETÓRIO'));
 }
@@ -11,30 +19,15 @@ async function pegaArquivo(caminhoDoArquivo){
     try{
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto));
+        console.log(extraiLinks(texto));
     } catch(erro){
         trataErro(erro)
     }
    
 }
 
-// promises com then()
-/*function pegaArquivo(caminhoDoArquivo){
-    const encoding = 'utf-8';
-    fs.promises
-        .readFile(caminhoDoArquivo, encoding)
-        .then((texto)=>console.log(chalk.green(texto)))
-        .catch(trataErro)
-}
 
-function pegaArquivo(caminhoDoArquivo){
-    const encoding = 'utf-8'
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=>{
-        if(erro){
-            trataErro(erro);
-        }
-        console.log(chalk.green(texto));
-    })
-}*/
+pegaArquivo('./arquivos/texto.md')
 
-pegaArquivo('./arquivos/texto.md');
+
+// \[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
