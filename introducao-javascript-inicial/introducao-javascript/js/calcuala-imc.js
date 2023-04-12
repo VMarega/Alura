b69018ed93f7d470092e66a6a51a7b0edcf64afe
VMarega <vinimarega@gmail.com>
2023-04-11 21:08:29 -0300

    aula 5

---
+++ introducao-javascript-inicial/introducao-javascript/js/calcuala-imc.js
@@ -0,0 +1,48 @@
+const titulo = document.querySelector('h1');
+titulo.textContent = "Aparecida Nutricionista";
+
+const pacientes = document.querySelectorAll(".paciente");
+
+for (let i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    const tdPeso = paciente.querySelector(".info-peso");
+    const peso = tdPeso.textContent;
+
+    const tdAltura = paciente.querySelector(".info-altura");
+    const altura = tdAltura.textContent;
+
+    const tdImc = paciente.querySelector(".info-imc")
+
+
+
+
+    let pesoEhValido = true;
+    let alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log('peso inválido')
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura Inválida"
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        let imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
+}
+
+function calculaImc(peso, altura){
+    let imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
---
+++ introducao-javascript-inicial/introducao-javascript/js/form.js
@@ -0,0 +1,48 @@
+const botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", (e)=>{
+    e.preventDefault();
+
+    const form = document.querySelector("#form-adiciona");
+    
+    let paciente = obtemPacienteDoFormulario(form);
+
+    
+
+    const pacienteTr = document.createElement("tr");
+    const nomeTd = document.createElement("td");
+    const pesoTd = document.createElement("td");
+    const alturaTd = document.createElement("td");
+    const gorduraTd = document.createElement("td");
+    const imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = calculaImc(peso,altura);
+   
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    const tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+})
+
+function obtemPacienteDoFormulario(form){
+
+    let paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.peso.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+}
