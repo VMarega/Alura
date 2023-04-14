80f17ca5d926b71a82d52dfaa282f4baa7858234
VMarega <vinimarega@gmail.com>
2023-04-13 23:31:17 -0300

    aula 8.2

--- introducao-javascript-inicial/introducao-javascript/js/filtra.js
+++ introducao-javascript-inicial/introducao-javascript/js/filtra.js
@@ -1,5 +1,24 @@
 const campoFiltro = document.querySelector("#filtrar-tabela");
 
-campoFiltro.addEventListener("input", ()=>{
-    console.log("digitaram no campo");
+campoFiltro.addEventListener("input", () => {
+
+    let pacientes = document.querySelectorAll(".paciente");
+
+    if (campoFiltro.value.length > 0) {
+        for (let i = 0; i < pacientes.length; i++) {
+            let paciente = pacientes[i];
+            let tdNome = paciente.querySelector(".info-nome");
+            let nome = paciente.textContent;
+            if (nome != this.value) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (let i = 0; i < pacientes.length; i++) {
+            let paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
 })
