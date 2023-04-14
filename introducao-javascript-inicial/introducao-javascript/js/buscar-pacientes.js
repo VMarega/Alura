eddcce35d155c081ad9033cb9a9064af82ee3094
VMarega <vinimarega@gmail.com>
2023-04-14 15:21:17 -0300

    Errors on request

--- introducao-javascript-inicial/introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript-inicial/introducao-javascript/js/buscar-pacientes.js
@@ -6,12 +6,20 @@ botaoBuscar.addEventListener("click", ()=>{
     xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json");
 
     xhr.addEventListener("load", ()=>{
-        let resposta = xhr.responseText;
-        let pacientesJSON = JSON.parse(resposta); 
-        
-        pacientesJSON.forEach((paciente)=>{
-            adicionaPacienteNaTabela(paciente);
-        })
+        const erroAjax = document.querySelector("#erro-ajax");
+        if(xhr.status == 200){
+            erroAjax.classList.add("invisivel");
+            let resposta = xhr.responseText;
+            let pacientesJSON = JSON.parse(resposta); 
+            
+            pacientesJSON.forEach((paciente)=>{
+                adicionaPacienteNaTabela(paciente);
+            })
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            erroAjax.classList.remove("invisivel");
+        }
         });
 
     xhr.send();
